96bfbd4513ebdec2bbe9bf270daf7fe41fe3972d
Learning-stud <jayahalpara123456@gmail.com>
2024-01-07 14:57:30 +0530

    ASSESMENT_1ST

--- PYTHON ASSESMENT_1ST/Bank_Management.py
+++ PYTHON ASSESMENT_1ST/Bank_Management.py
@@ -10,22 +10,29 @@ def display_menu():
     print("3. Exit")
 
 def main():
+    # calling  both function at a time of options
     banker = Banker()
     customer = Customer()
 
+
+# Displaying selection type.....
     while True:
         display_menu()
         choice = input("Enter your choice (1-3): ")
-
+# when user press 1 and 2 executing the opration....
         if choice == '1':
             banker.execute_operations()
+
         elif choice == '2':
             customer.execute_operations()
+        # when User  Press 3 the Management system will be closed.....
         elif choice == '3':
             print("Exiting the Bank Management System. Goodbye!")
             break
         else:
             print("Invalid choice. Please enter a valid option (1-3).")
+
+        # automatically called the function
 
 if __name__ == "__main__":
     main()
