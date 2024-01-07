ccc96ae1f53ab0fc4d5e8687ae66e07eb0a1c5da
Learning-stud <jayahalpara123456@gmail.com>
2024-01-07 14:58:11 +0530

    ASSESMENT_1ST -> Customer

--- PYTHON ASSESMENT_1ST/Customer.py
+++ PYTHON ASSESMENT_1ST/Customer.py
@@ -9,6 +9,8 @@ class Customer:
         self.account_balance = 0.0
         self.log_file = 'transaction_log.txt'
 
+        # Types Of Operation For Customer
+
         self.operations = {
             '1': self.withdraw_amount,
             '2': self.deposit_amount,
@@ -16,10 +18,15 @@ class Customer:
         }
 
 
-
+        # Function to withdraw amount .......
     def withdraw_amount(self):
         print("\nCustomer: Withdrawing amount.")
+
+        # The Amount Will be in An float values because  if user want to clear all account
         withdrawal_amount = float(input("Enter the amount to withdraw: "))
+
+        #If withdraw amount is less then account balance then the remaining balance will be shown and
+        #else the amount is insufficient withdraw cancelled
 
         if withdrawal_amount <= self.account_balance:
             self.account_balance -= withdrawal_amount
@@ -30,7 +37,8 @@ class Customer:
 
 
 
-
+      # function To deposit money
+        #When the user added the amount then value will be added  and print amount added or deposite
     def deposit_amount(self):
         print("\nCustomer: Depositing amount.")
         deposit_amount = float(input("Enter the amount to deposit: "))
@@ -40,14 +48,14 @@ class Customer:
         self.log_transaction(f"Deposited 💰{deposit_amount}")
 
 
-
+    #Showing Balance
 
     def view_balance(self):
         print("\nCustomer: Viewing balance.")
         print(f"Current Balance: 💰{self.account_balance}")
 
 
-
+    # function to  execute oprations for what to do .......
     def execute_operations(self):
         while True:
             print("\nCustomer Operations Menu")
@@ -57,11 +65,11 @@ class Customer:
             print("4. Go back to main menu")
 
             choice = input("Enter your choice (1-4): ")
-
+            # when User press 4 the he will return to main menu
             if choice == '4':
                 print("Returning to the main menu.")
                 break
-
+            # if opration is not satisfied then invalid choice
             operation = self.operations.get(choice)
             if operation:
                 operation()
@@ -69,7 +77,7 @@ class Customer:
                 print("Invalid choice. Please enter a valid option (1-4).")
 
 
-
+        # file to save data 
     def log_transaction(self, message):
         """Log transaction details to a file."""
         with open(self.log_file, 'a') as file:
